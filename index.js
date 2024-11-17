60bda669b50166ceb2fa837d9f9d95faed5d550d
Qyxin06 <qyxin06@gmail.com>
2024-11-17 22:03:26 +0800

    11.17, before arrays

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 let firstCard = 10
 let secondCard = 4
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
+let messageEl = document.querySelector("#message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
 function startGame() {
+    renderGame()
+}
+function renderGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard 
+    sumEl.textContent = "Sum : " + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card? "
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "Wohoo! You've got Blackjack! "
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game! "
         isAlive = false
     }
-    console.log(message)    
+    messageEl.textContent = message  
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    let thirdCard = 6
+    sum += thirdCard
+    renderGame()
 }
